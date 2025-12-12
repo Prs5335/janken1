a1cb9a89ebc6053aad625a40e371f599be63bdb4
Prs5335 <2312058@i-seifu.jp>
2025-12-12 11:28:54 +0900

    Add initial HTML structure for index page

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>ゲーム一覧</title>
+</head>
+<body>
+    <h1>ゲーム一覧</h1>
+    <ul>
+        <li><a href="./janken.php">じゃんけんゲーム</a></li>
+    </ul>
+</body>
+</html>
